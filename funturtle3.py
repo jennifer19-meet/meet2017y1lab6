ba40d8faf8e4c85be15320ef56010b3e8f87f8ee
jennifer19-meet <jennifer19@meet.mit.edu>
2017-07-31 18:30:46 +0300

    Stopping here

--- funturtle3.py
+++ funturtle3.py
@@ -1,7 +1,18 @@
+import turtle
+turtle.shape = "turtle"
+
+UP_ARROW = "Up"
+LEFT_ARROW = "Left"
+DOWN_ARROW = "Down"
+RIGHT_ARROW = "Right"
+SPACEBAR = "space"
+
 UP = 0
 LEFT = 1
 DOWN = 2
 RIGHT = 3
+
+
 direction = UP
 
 def up() :
@@ -20,4 +31,10 @@ def right() :
     global direction
     direction = RIGHT
     print ("you pressed right") 
+
+turtle.onkeypress (up, UP_ARROW)
+turtle.onkeypress (left, LEFT_ARROW)
+turtle.onkeypress (down, DOWN_ARROW)
+turtle.onkeypress (right, RIGHT_ARROW)
+turtle.listen()
 
